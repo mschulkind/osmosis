05d0ad218338f7f29bbdb6d10d67e8bcef594665
Matthew Schulkind <mschulkind@gmail.com>
2014-11-23 23:49:11 -0500

    Added sound file loading.

--- src/clj/osmosis/server.clj
+++ src/clj/osmosis/server.clj
@@ -7,6 +7,12 @@
             [net.cgrand.reload]
             [ring.adapter.jetty :refer [run-jetty]]))
 
+(defn sound-filenames []
+  (for [f (map #(.getName %) 
+               (.listFiles (io/file (io/resource "sound/processed"))))]
+    (let [base (last (re-find #"(.*)\.mp3" f))]
+      {:base base :full (str "sound/" f)})))
+
 (en/deftemplate page (io/resource "public/index.html") []
   [:body] 
   (en/append 
@@ -20,7 +26,14 @@
                        :href "css/bootstrap.css"}]))
 
     (en/append
-      (en/html [:script {:type "text/javascript" :src "js/react.js"}]))))
+      (en/html [:script {:type "text/javascript" :src "js/react.js"}]))
+    
+    (en/append
+      (en/html
+        (for [f (sound-filenames)]
+          [:audio {:src (:full f) 
+                   :preload "auto" 
+                   :id (str "sample-" (:base f))}])))))
 
 (defroutes routes
   (resources "/css" {:root "public/css"})
@@ -31,6 +44,8 @@
   (resources "/js" {:root "vendor/bootstrap/js"})
   (resources "/fonts" {:root "vendor/bootstrap/fonts"})
   (resources "/css" {:root "vendor/bootstrap/css"})
+
+  (resources "/sound" {:root "sound/processed"})
 
   (GET "/*" req (page)))
 
