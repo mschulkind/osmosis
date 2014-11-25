fb08d4ebabd0e2e148cc59c360ddd3b8449f5521
Matthew Schulkind <mschulkind@gmail.com>
2014-11-24 23:10:55 -0500

    Give the samples a fade out.

--- resources/sound/process.sh
+++ resources/sound/process.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for i in original/*; do 
-  sox $i -t wav - fade 0 1 | lame - processed/$(basename ${i%%.wav}).mp3
+  sox $i -t wav - fade 0 1.2 0.4 | lame - processed/$(basename ${i%%.wav}).mp3
 done
+mp3gain -r -k processed/*
