19fb8578b6f81a0eeb4b1f038b2be8e5309b9d2b
Matthew Schulkind <mschulkind@gmail.com>
2014-11-26 01:57:21 -0500

    Require leiningen 2.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :min-lein-version "2.0.0"
+
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -43,7 +45,7 @@
 
   :haml {:src "src/haml"
          :output-directory "resources/public/html"}
-         
+
 
   :sass {:src "src/sass"
          :output-directory "resources/public/css"}
